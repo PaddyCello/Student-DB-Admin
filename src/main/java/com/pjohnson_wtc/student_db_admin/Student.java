1b60c982a37450bc4b550266ba1e23f0d4ee09ca
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2021-12-09 10:36:17 +0000

    Add classes and mockito dependencies

---
+++ src/main/java/com/pjohnson_wtc/student_db_admin/Student.java
@@ -0,0 +1,5 @@
+package com.pjohnson_wtc.student_db_admin;
+
+public class Student {
+
+}
---
+++ src/test/java/com/pjohnson_wtc/student_db_admin/AdminTest.java
@@ -0,0 +1,14 @@
+package com.pjohnson_wtc.student_db_admin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdminTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
