598d9edd8ffe6dcebe354a54eb786ce386ee6bb0
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2021-12-10 16:30:01 +0000

    Getting to know Scanner objects

--- src/main/java/com/pjohnson_wtc/student_db_admin/Admin.java
+++ src/main/java/com/pjohnson_wtc/student_db_admin/Admin.java
@@ -1,9 +1,23 @@
 package com.pjohnson_wtc.student_db_admin;
 
+import java.util.Scanner;
+
 public class Admin {
+	
+	public int createStudent() {
+		// TODO Auto-generated method stub
+		return 6;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		
+		String name = scanner.nextLine();
+		scanner.close();
+		
+		System.out.println("Nice to meet you, " + name);
 
 	}
 
--- src/test/java/com/pjohnson_wtc/student_db_admin/AdminTest.java
+++ src/test/java/com/pjohnson_wtc/student_db_admin/AdminTest.java
@@ -3,12 +3,23 @@ package com.pjohnson_wtc.student_db_admin;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AdminTest {
+	
+	@Mock
+	Student student;
+	
+	@InjectMocks
+	Admin admin;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateStudent() {
+		assertEquals(6, admin.createStudent());
 	}
 
 }
