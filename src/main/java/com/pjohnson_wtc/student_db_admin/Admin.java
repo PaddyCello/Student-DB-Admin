1225fe3da2bda202920661750ab32b873c991f24
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2021-12-13 10:59:44 +0000

    Using InputStream as argument for createStudent to mock console input

--- src/main/java/com/pjohnson_wtc/student_db_admin/Admin.java
+++ src/main/java/com/pjohnson_wtc/student_db_admin/Admin.java
@@ -1,12 +1,34 @@
 package com.pjohnson_wtc.student_db_admin;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Admin {
 	
-	public int createStudent() {
-		// TODO Auto-generated method stub
-		return 6;
+	private List<Student> allStudents = new ArrayList<Student>();
+	
+	//Method for creating new Student from user input
+	public int createStudent(InputStream inputStream) {
+		Scanner scanner = new Scanner(inputStream);
+		System.out.println("What is your first name?");
+		String firstName = scanner.nextLine();
+		System.out.println("What is your last name?");
+		String lastName = scanner.nextLine();
+		System.out.println("What is your year?");
+		
+		int year = Integer.parseInt(scanner.nextLine());
+		scanner.close();
+		
+		Student student = new Student(firstName, lastName, year);
+		allStudents.add(student);
+		
+		return year;
+	}
+	
+	public List<Student> getAllStudents() {
+		return allStudents;
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/pjohnson_wtc/student_db_admin/Student.java
+++ src/main/java/com/pjohnson_wtc/student_db_admin/Student.java
@@ -1,5 +1,17 @@
 package com.pjohnson_wtc.student_db_admin;
 
 public class Student {
-
+	private String firstName;
+	private String lastName;
+	private int year;
+	
+	public Student(String firstName, String lastName, int year) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.year = year;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
 }
